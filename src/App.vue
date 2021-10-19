c2419cb55b28840961b96ceb7a684cc6f15a551a
sunweidong <2363140194@qq.com>
2021-10-19 17:28:55 +0800

    vue3

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="container">
+    <h3 class="title">
+      Hello <span class="red">{{ name }}</span>
+    </h3>
+    <HelloWorld />
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+import HelloWorld from './components/HelloWorld.vue';
+import { name } from './common/const.js';
+
+export default defineComponent({
+  components: {
+    HelloWorld,
+  },
+  data() {
+    return {
+      name,
+    };
+  },
+});
+</script>
+
+<style lang="less">
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.title {
+  text-align: center;
+  .red {
+    color: red;
+  }
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,7 @@
+<template>
+  <img :src="srcLogo" alt="logo" />
+</template>
+
+<script setup>
+import srcLogo from '../assets/logo.png';
+</script>
